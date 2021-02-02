b0b3896471f45e835db1d427a1a6e4bb16b8442f
Dukul Howlader Mohin <dukul.howlader30@gmail.com>
2021-02-02 21:19:43 +0600

    null vs udefined clear

---
+++ null vs undefined.js
@@ -0,0 +1,21 @@
+undefined types
+1.let pakhi;
+
+2. function add(num1, num2){
+    console.log(num1+num2);
+    return
+}
+
+3.function add(num1,num2){
+    console.log(num1+num2);
+}
+
+const result = add(13);
+
+4.const premik = {name:"smart dude", phone:14524};
+console.log(premik.gf);
+
+5.let fun = undefined;
+console.log(fun);
+
+null types
---
+++ true-false.js
@@ -0,0 +1,12 @@
+//false occurring condition
+// 0
+//""
+//undefined
+//null
+//NaN
+const name = "allu";
+if (name.length >0) {
+    console.log("Condition is true ")
+} else {
+    console.log('Condition is false')
+}
